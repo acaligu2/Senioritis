d39dfeeae5fb6a88284bc54f8151df99c34a8bb8
Anthony <anthony@Anthonys-Mac.local>
2019-04-20 19:26:16 -0400

    Adding background

--- Senioritis/GameViewController.swift
+++ Senioritis/GameViewController.swift
@@ -16,14 +16,15 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
+            
+            let scene = GameScene(size: CGSize(width: 1280, height: 720))
+            
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
             
